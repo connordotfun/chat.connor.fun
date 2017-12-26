3212fae2d7c541b055a2eefae863ee248584754e
Ethan Hunter <fc.spaceman@gmail.com>
2017-12-26 15:53:32 -0700

    add consistent response pattern

---
+++ controllers/response.go
@@ -0,0 +1,12 @@
+package controllers
+
+
+type ResponseError struct {
+	Type string `json:"type"`
+	Message string `json:"message"`
+}
+
+type Response struct {
+	Error ResponseError `json:"error"`
+	Data interface{} `json:"data"`
+}
--- controllers/user.go
+++ controllers/user.go
@@ -12,34 +12,58 @@ import (
 func CreateUser(c echo.Context) error {
 	var u model.User
 	if err := c.Bind(&u); err != nil {
-		return c.JSON(http.StatusInternalServerError, err)
+		return c.JSON(http.StatusBadRequest, Response{
+			Error: ResponseError{Type: "BAD_BINDING", Message: err.Error()},
+			Data: nil,
+		})
 	}
 	hashedSecret, err := bcrypt.GenerateFromPassword([]byte(u.Secret), bcrypt.DefaultCost)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
+		return c.JSON(http.StatusInternalServerError, Response{
+			Error: ResponseError{Type: "BAD_PASSWORD", Message: err.Error()},
+			Data: nil,
+		})
 	}
 	u.Secret = string(hashedSecret)
 	createdUser, err := user.Repo.Create(u)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
+		return c.JSON(http.StatusInternalServerError, Response{
+			Error: ResponseError{Type: "USER_CREATE_FAILED", Message: err.Error()},
+			Data: nil,
+		})
 	}
 	createdUser.Secret = "" //don't return secret
-	return c.JSON(http.StatusCreated, createdUser)
+	return c.JSON(http.StatusCreated, Response{
+		Error: nil,
+		Data: createdUser,
+	})
 }
 
 func LoginUser(c echo.Context) error {  //TODO: generate JWTs
 	var toLoginUser model.User
 	if err := c.Bind(&toLoginUser); err != nil {
-		return c.NoContent(http.StatusBadRequest)
+		return c.JSON(http.StatusBadRequest, Response{
+			Error: ResponseError{Type: "BAD_BINDING", Message: err.Error()},
+			Data: nil,
+		})
 	}
 	matchedUser, err := user.Repo.GetByUsername(toLoginUser.Username)
 	if err != nil { //TODO: Handle user not found case
-		return err
+		return c.JSON(http.StatusBadRequest, Response{
+			Error: ResponseError{Type: "USER_NOT_FOUND", Message: err.Error()},
+			Data: nil,
+		})
 	}
 
 	if bcrypt.CompareHashAndPassword([]byte(matchedUser.Secret), []byte(toLoginUser.Secret)) != nil {
-		return c.NoContent(http.StatusUnauthorized)
+		return c.JSON(http.StatusUnauthorized, Response{
+			Error: ResponseError{Type: "PASSWORD_MATCH_FAILED", Message: err.Error()},
+			Data: nil,
+		})
 	} else {
-		return c.NoContent(http.StatusOK)
+		return c.JSON(http.StatusOK, Response{
+			Error: nil,
+			Data: model.User{Id: matchedUser.Id, Username: matchedUser.Username, Secret: ""},
+		})
 	}
 }
