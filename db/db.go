0be03b8c95dc338364462027e57689ded43e64d6
Ethan Hunter <fc.spaceman@gmail.com>
2017-12-26 18:16:01 -0700

    remove unused file

--- db/db.go
+++
@@ -1,9 +0,0 @@
-package db
-
-type Repository interface{
-	Create(interface{}) error
-	Update(interface{}) error
-	GetAll() ([]*interface{}, error)
-	GetById(id int) (interface{}, error)
-	Delete(interface{}) error
-}
