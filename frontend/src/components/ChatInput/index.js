6c5bb952e4a3b538ca4c6e86182c607e28362087
Connor Hudson <cohu3291@colorado.edu>
2018-01-01 00:23:08 -0700

    rename input to chatinput

---
+++ frontend/src/components/ChatInput/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './index.css'
+
+class ChatInput extends Component {
+    render() {
+        return (
+            <div className="ChatInput">
+                <input className="concave" type="text" />
+            </div>
+        )
+    }
+}
+
+export default ChatInput
