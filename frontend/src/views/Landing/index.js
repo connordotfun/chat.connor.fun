ef3bc929a0d1f8951f07a651c75a3dd078732c76
Connor Hudson <cohu3291@colorado.edu>
2018-01-01 17:52:13 -0700

    Initial implementation of logged out landing page

--- frontend/src/views/Landing/index.js
+++ frontend/src/views/Landing/index.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
-import Header from '../../components/Header'
-import Messages from '../../components/Messages'
-import Input from '../../components/Input'
-import './index.css';
+import { observer, inject } from 'mobx-react'
 
+import UserForm from '../../components/UserForm'
+import './index.css'
+
+@inject('commonStore')
+@observer
 class Landing extends Component {
     render() {
         return (
             <div className="Landing convex">
                 <h1>Welcome!</h1>
                 <p><em>chat.connor.fun</em> is a cool hangout place.</p>
+                <UserForm />
+                <p>If we gotta token, it's {this.props.commonStore.token}</p>
             </div>
         )
     }
--- frontend/src/views/Landing/index.test.js
+++ frontend/src/views/Landing/index.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Chat from './';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Landing from './'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Chat />, div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<Landing />, div)
+})
