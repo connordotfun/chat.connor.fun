ade9679e2feff33d814aedbf1850a977296cded7
Ethan Hunter <fc.spaceman@gmail.com>
2018-01-02 15:56:42 -0700

    fix messages query

--- controllers/messages_controller.go
+++ controllers/messages_controller.go
@@ -9,32 +9,54 @@ import (
 	"strconv"
 )
 
+func getMessagesRoom(c echo.Context, messagesRepo db.MessagesRepository, roomIdStr string, count int) error {
+	roomId, err := uuid.FromString(roomIdStr)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, model.NewErrorResponse("BAD_QUERY"))
+	}
+	var messages []*model.Message
+	if count <= 0 {
+		messages, err = messagesRepo.GetTopByRoom(roomId, count)
+	} else {
+		messages, err = messagesRepo.GetByRoomId(roomId)
+	}
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, model.NewErrorResponse("RETRIEVE_FAILED"))
+	}
+	return c.JSON(http.StatusOK, model.NewDataResponse(messages))
+}
+
+func getMessagesUser(c echo.Context, messagesRepo db.MessagesRepository, userIdStr string, count int) error {
+	return nil
+}
+
+func getMessagesUsersAndRoom(c echo.Context, messagesRepo db.MessagesRepository, roomIdStr string, userIdStr string, count int) error {
+	return nil
+}
+
+func getAllMessages(c echo.Context, messagesRepo db.MessagesRepository, count int) error {
+	return nil
+}
+
 func GetMessages(messagesRepo db.MessagesRepository) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		roomIdStr := c.Param("room")
-		roomId, err := uuid.FromString(roomIdStr)
+		roomIdStr := c.QueryParam("room_id")
+		userIdStr := c.QueryParam("user_id")
+
+		count, err := strconv.Atoi(c.QueryParam("count"))
 		if err != nil {
-			return c.JSON(http.StatusBadRequest, model.NewErrorResponse("BAD_ID"))
+			count = -1
 		}
 
-		var messages []*model.Message
-		if countStr := c.QueryParam("count"); countStr != "" {
-			count, err := strconv.Atoi(countStr)
-			if err != nil {
-				return c.JSON(http.StatusBadRequest, model.NewErrorResponse("BAD_QUERY"))
-			}
-			messages, err = messagesRepo.GetTopByRoom(roomId, count)
-			if err != nil {
-				return c.JSON(http.StatusInternalServerError, model.NewErrorResponse("FAILED_RETRIEVE"))
-			}
-		} else {
-			messages, err = messagesRepo.GetByRoomId(roomId)
-			if err != nil {
-				return c.JSON(http.StatusInternalServerError, model.NewErrorResponse("FAILED_RETRIEVE"))
-			}
+		if roomIdStr != "" {
+			return getMessagesRoom(c, messagesRepo, roomIdStr, count)
+		} else if userIdStr != "" {
+			return getMessagesUser(c, messagesRepo, userIdStr, count)
+		} else if roomIdStr != "" && userIdStr != "" {
+			return getMessagesUsersAndRoom(c, messagesRepo, roomIdStr, userIdStr, count)
 		}
-
-		return c.JSON(http.StatusOK, model.NewDataResponse(messages))
+		return getAllMessages(c, messagesRepo, count)
 	}
 }
 
