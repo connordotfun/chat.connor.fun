f8fec6e192c9a6b803ff49d97869c49a5fcd5ab2
Ethan Hunter <fc.spaceman@gmail.com>
2018-01-04 23:00:30 -0700

    fix permission and add get room method

--- controllers/rooms_controller.go
+++ controllers/rooms_controller.go
@@ -6,10 +6,29 @@ import (
 	"github.com/aaronaaeng/chat.connor.fun/controllers/chat"
 	"net/http"
 	"github.com/aaronaaeng/chat.connor.fun/model"
+	"errors"
 )
 
 func GetNearbyRooms(roomsRepo db.RoomsRepository) echo.HandlerFunc {
 	return nil
+}
+
+func getRoomMembersList(hub *chat.Hub) ([]model.User, error) {
+	if hub == nil {
+		return make([]model.User, 0), nil
+	}
+
+	usersChan := make(chan []model.User)
+
+	hub.GetUserList <- usersChan
+
+	users, ok := <- usersChan
+	if !ok {
+		return nil, errors.New("room in illegal state (shutting-down or shutdown)")
+	}
+	close(usersChan)
+
+	return users, nil
 }
 
 func GetRoomMembers(hubMap *chat.HubMap) echo.HandlerFunc { //there's no good way to test this rn
@@ -20,14 +39,38 @@ func GetRoomMembers(hubMap *chat.HubMap) echo.HandlerFunc { //there's no good wa
 			return c.JSON(http.StatusOK, model.NewDataResponse(make([]*model.User, 0)))
 		}
 
-		usersChan := make(chan []model.User)
-		hub.GetUserList <- usersChan
-
-		users, ok := <- usersChan
-		if !ok {
+		users, err := getRoomMembersList(hub)
+		if err != nil {
 			return c.JSON(http.StatusInternalServerError, model.NewErrorResponse("HUB_SHUTDOWN"))
 		}
-		close(usersChan)
 		return c.JSON(http.StatusOK, model.NewDataResponse(users))
+	}
+}
+
+func GetRoom(roomsRepository db.RoomsRepository, hubMap *chat.HubMap) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		roomName := c.Param("room")
+		hub, ok := hubMap.Load(roomName)
+		room := hub.Room
+		if !ok {
+			roomFromDb, err := roomsRepository.GetByName(roomName)
+			if err != nil {
+				return c.JSON(http.StatusInternalServerError, model.NewErrorResponse("RETRIEVE_FAILED"))
+			}
+			if roomFromDb == nil {
+				return c.JSON(http.StatusNotFound, model.NewErrorResponse("ROOM_DNE"))
+			}
+			room = roomFromDb
+		}
+
+		roomMembers, err := getRoomMembersList(hub)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, model.NewErrorResponse("HUB_SHUTDOWN"))
+		}
+
+		roomToReturn := *room
+		roomToReturn.Members = roomMembers
+
+		return c.JSON(http.StatusOK, model.NewDataResponse(roomToReturn))
 	}
 }
--- model/chat_room.go
+++ model/chat_room.go
@@ -5,6 +5,6 @@ import "github.com/satori/go.uuid"
 type ChatRoom struct {
 	Id uuid.UUID `json:"id"`
 	Name string `json:"name"`
-	Members []*User `json:"members"`
+	Members []User `json:"members"`
 	//maybe geolocation data
 }
