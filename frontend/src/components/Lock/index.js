5f3fb2198f217294bda8396fbf109abadb973dcf
Connor Hudson <cohu3291@colorado.edu>
2018-01-10 21:15:31 -0700

    Add Lock component

---
+++ frontend/src/components/Lock/index.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * <Lock to={"normalUser"}>
+ *  <Component />
+ * </Lock>
+ * 
+ */
+
+import React, { Component } from 'react'
+
+class Lock extends Component {
+    /* if provided props.user role is at least props.minRole, render children */
+    validRoles = [
+        'anonUser',
+        'normalUser',
+        'unverifiedUser',
+        'admin'
+    ]
+
+    render() {
+        const userRoles = this.props.user.roles.map(role => role.name)
+        const minRole = this.props.minRole
+        if (validRoles.indexOf(minRole) != -1 && userRoles.indexOf(minRole) != -1) {
+            return this.props.children
+        } else {
+            return null
+        }
+    }
+}
