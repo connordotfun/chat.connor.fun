d8b40a850ee69480ad897313f0bd0922ff407f35
Connor Hudson <cohu3291@colorado.edu>
2018-01-13 12:44:11 -0700

    Fix issues, add unverified user pages

--- frontend/src/stores/authStore.js
+++ frontend/src/stores/authStore.js
@@ -9,6 +9,7 @@ class AuthStore {
   @observable values = {
     username: '',
     password: '',
+    email: ''
   };
 
   @action setUsername(username) {
@@ -19,9 +20,14 @@ class AuthStore {
     this.values.password = password
   }
 
+  @action setEmail(email) {
+    this.values.email = email
+  }
+
   @action reset() {
     this.values.username = ''
     this.values.password = ''
+    this.values.email = ''
   }
 
   @action login() {
@@ -48,16 +54,30 @@ class AuthStore {
     this.errors = undefined
     axios.post('/api/v1/users', {
       username: this.values.username,
-      secret: this.values.password
+      secret: this.values.password,
+      email: this.values.email
     })
-    .then((res) => {
-      this.login()
-    })
+    .then((res) => console.log(res.data))
     .catch(action((err) => {
       this.errors = err.response.data.error
       throw this.errors
     }))
     .finally(action(() => {
+      this.login()
+      this.inProgress = false
+    }))
+  }
+
+  @action verifyAccount(code) {
+    this.inProgress = true
+    this.errors = undefined
+    axios.put('/verifications/accountverification', {code})
+    .catch(action((err) => {
+      this.errors = err.response.data.error
+      throw this.errors
+    }))
+    .finally(action(() => {
+      this.login()
       this.inProgress = false
     }))
   }
