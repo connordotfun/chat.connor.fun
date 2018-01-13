f971e3a235be1f49067078a51d697109e7a4de9a
Connor Hudson <cohu3291@colorado.edu>
2018-01-13 12:44:11 -0700

    Fix issues, add unverified user pages

---
+++ frontend/src/views/VerifyLanding/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { observer, inject } from 'mobx-react'
+
+import './index.css'
+
+@inject('commonStore')
+@observer
+class VerifyLanding extends Component {
+    componentWillMount() {
+        this.props.history.replace('/')
+    }
+    
+    render() {
+        return (
+            <div className="VerifyLanding convex">
+                <h1>Welcome back, {this.props.commonStore.user.username}!</h1>
+                <p>You need to verify your account before using <em>chat.connor.fun</em>.</p>
+                <p>Check your email or click here to resend the verification message.</p>
+            </div>
+        )
+    }
+}
+
+export default VerifyLanding
