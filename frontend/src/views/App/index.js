974ff3c3bf03bfd5fc7558c9055d8b88283bc5b7
Connor Hudson <cohu3291@colorado.edu>
2018-01-13 11:42:23 -0700

    Use Lock for role-specific routing

--- frontend/src/views/App/index.js
+++ frontend/src/views/App/index.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react'
 import { observer, inject } from 'mobx-react'
 import { Switch, Route } from 'react-router-dom'
 
-import Chat from '../Chat'
 import Landing from '../Landing'
+import Login from '../Login'
+import Register from '../Register'
+
+import Chat from '../Chat'
 import Home from '../Home'
+
+import Lock from '../../components/Lock/HOC'
 
 @inject('commonStore')
 @observer
@@ -13,15 +18,17 @@ class App extends Component {
         if (this.props.commonStore.token) {
             return (
                 <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/at/:room" component={Chat}/>
-                    <Route component={Home}/>
+                    <Route exact path="/" component={Lock(Home, "normalUser", this.props.commonStore.user)}/>
+                    <Route path="/at/:room" component={Lock(Chat, "normalUser", this.props.commonStore.user)}/>
+                    <Route component={Lock(Home, "normalUser", this.props.commonStore.user)}/>
                 </Switch>
             )
         } else {
             return (
                 <Switch>
-                    <Route exact path="/" component={Landing}/>
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
                     <Route component={Landing} />
                 </Switch>
             )
