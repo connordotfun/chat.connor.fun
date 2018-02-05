1153deaf8f80a480316b9859bd0849cf90d05199
Connor Hudson <cohu3291@colorado.edu>
2018-02-04 23:48:09 -0700

    Restyle login page

--- frontend/src/views/Login/index.js
+++ frontend/src/views/Login/index.js
@@ -4,14 +4,27 @@ import { observer, inject } from 'mobx-react'
 import UserForm from '../../components/UserForm'
 
 import './index.css'
+import logo from '../../logo.png'
 
 @inject('commonStore')
 @observer
 class Login extends Component {
+    componentWillMount() {
+        document.getElementsByTagName('body')[0].classList.add('no-padding')
+    }
+
     render() {
         return (
-            <div className="Login convex">
-                <UserForm />
+            <div className="Login">
+                <div className="branding">
+                    <h1 className="brand-title">Chat</h1>
+                    <h2 className="subtitle">a <img className="logo" src={logo} alt="connor.fun" /> product.</h2>
+                </div>
+                <div className="account-container">
+                    <div className="login-box">
+                        <UserForm />
+                    </div>
+                </div>
             </div>
         )
     }
